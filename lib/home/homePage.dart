16ce168e30b75ac4f53c226162a6f9ada29e3d29
thawatchai_m <demonridercos@gmail.com>
2024-10-08 13:35:37 +0700

    Edit Thawatchai V5

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -1,6 +1,4 @@
 import 'package:cookingapp/Store/Graphs/graphsPage.dart';
-import 'package:cookingapp/Store/Graphs/test3.dart';
-import 'package:cookingapp/Store/Graphs/testGraphs.dart';
 import 'package:cookingapp/constants.dart';
 import 'package:cookingapp/home/detailFoodPage.dart';
 import 'package:cookingapp/home/services/homeApi.dart';
@@ -121,65 +119,65 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ),
                           ),
-                          Image.asset('assets/icons/cam.png'),
-                          SizedBox(
-                            height: size.height * 0.05,
-                            width: size.width * 0.225,
-                            child: DropdownButtonHideUnderline(
-                              child: DecoratedBox(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(8),
-                                  border: Border.all(color: Colors.white, width: 2),
-                                ),
-                                child: DropdownButton2<String>(
-                                  isExpanded: true,
-                                  hint: Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      'เลือกสินค้า',
-                                      style: TextStyle(
-                                        fontSize: 11,
-                                        color: red1,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  items: items
-                                      .map((String item) => DropdownMenuItem<String>(
-                                            value: item,
-                                            child: Text(
-                                              item,
-                                              style: TextStyle(fontSize: 14, color: red1),
-                                            ),
-                                          ))
-                                      .toList(),
-                                  value: selectedValue,
-                                  onChanged: (String? value) {
-                                    setState(() {
-                                      selectedValue = value!;
-                                    });
-                                    // getlistCategories(name: selectedValue);
-                                  },
-                                  buttonStyleData: ButtonStyleData(
-                                    padding: EdgeInsets.symmetric(horizontal: size.width * 0.02),
-                                    width: size.width * 0.1,
-                                  ),
-                                  menuItemStyleData: MenuItemStyleData(
-                                    height: size.height * 0.1,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: size.width * 0.001,
-                          ),
+                          Image.asset('assets/icons/cam.png', color: red1,),
+                          // SizedBox(
+                          //   height: size.height * 0.05,
+                          //   width: size.width * 0.225,
+                            // child: DropdownButtonHideUnderline(
+                            //   child: DecoratedBox(
+                            //     decoration: BoxDecoration(
+                            //       color: Colors.white,
+                            //       borderRadius: BorderRadius.circular(8),
+                            //       border: Border.all(color: Colors.white, width: 2),
+                            //     ),
+                            //     child: DropdownButton2<String>(
+                            //       isExpanded: true,
+                            //       hint: Align(
+                            //         alignment: Alignment.centerLeft,
+                            //         child: Text(
+                            //           'เลือกสินค้า',
+                            //           style: TextStyle(
+                            //             fontSize: 11,
+                            //             color: red1,
+                            //             fontWeight: FontWeight.bold,
+                            //           ),
+                            //         ),
+                            //       ),
+                            //       items: items
+                            //           .map((String item) => DropdownMenuItem<String>(
+                            //                 value: item,
+                            //                 child: Text(
+                            //                   item,
+                            //                   style: TextStyle(fontSize: 14, color: red1),
+                            //                 ),
+                            //               ))
+                            //           .toList(),
+                            //       value: selectedValue,
+                            //       onChanged: (String? value) {
+                            //         setState(() {
+                            //           selectedValue = value!;
+                            //         });
+                            //         // getlistCategories(name: selectedValue);
+                            //       },
+                            //       buttonStyleData: ButtonStyleData(
+                            //         padding: EdgeInsets.symmetric(horizontal: size.width * 0.02),
+                            //         width: size.width * 0.1,
+                            //       ),
+                            //       menuItemStyleData: MenuItemStyleData(
+                            //         height: size.height * 0.1,
+                            //       ),
+                            //     ),
+                            //   ),
+                            // ),
+                          //),
+                          // SizedBox(
+                          //   width: size.width * 0.001,
+                          // ),
                           GestureDetector(
                             onTap: () {},
                             child: Container(
                               height: size.height * 0.05,
-                              width: size.width * 0.164,
+                              width: size.width * 0.264,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(10),
                                 color: red1,
